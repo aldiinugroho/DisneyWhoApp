75ac282411f877952a5c65f55578eef9925370a8
aldiinugroho <nugroho.aaldi@gmail.com>
2022-12-12 11:45:27 +0700

    setup app

---
+++ DisneyWhoApp/src/pages/MainViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MainViewController.swift
+//  DisneyWhoApp
+//
+//  Created by aldinugroho on 12/12/22.
+//
+
+import Foundation
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+        layout()
+    }
+}
+
+extension MainViewController {
+    private func setup() {
+    }
+    
+    private func layout() {
+        
+    }
+}
